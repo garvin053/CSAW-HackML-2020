0e3b53f69b39101f413574c1c1f5f767ec4d17f8
garvin053 <garvin00503@gmail.com>
2021-12-15 21:34:56 -0500

    lab3

--- lab3/eval.py
+++ lab3/eval.py
@@ -5,7 +5,8 @@ import numpy as np
 
 clean_data_filename = str(sys.argv[1])
 poisoned_data_filename = str(sys.argv[2])
-model_filename = str(sys.argv[3])
+bd_model_filename = str(sys.argv[3])
+gd_model_filename = str(sys.argv[4])
 
 def data_loader(filepath):
     data = h5py.File(filepath, 'r')
@@ -19,7 +20,10 @@ def main():
     cl_x_test, cl_y_test = data_loader(clean_data_filename)
     bd_x_test, bd_y_test = data_loader(poisoned_data_filename)
 
-    bd_model = keras.models.load_model(model_filename)
+    bd_model = keras.models.load_model(bd_model_filename)
+
+    gd_model = keras.models.load_model(gd_model_filename)
+
 
     cl_label_p = np.argmax(bd_model.predict(cl_x_test), axis=1)
     clean_accuracy = np.mean(np.equal(cl_label_p, cl_y_test))*100
